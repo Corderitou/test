7cbb93e1f0a1b2aed42fa94f11591e9084af70c9
Corderitou <107023182+Corderitou@users.noreply.github.com>
2024-04-28 02:42:32 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyComponent from "./mqtt.jsx"
+import test1 from "test1.jsx"
 
 function App() {
   return (
