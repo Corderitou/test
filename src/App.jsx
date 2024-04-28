aeb6ac53e961485ea868c91ed647ed5399344572
Corderitou <107023182+Corderitou@users.noreply.github.com>
2024-04-28 02:44:30 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import test1 from "test1.jsx"
+import test1 from "./test1.jsx"
 
 function App() {
   return (
